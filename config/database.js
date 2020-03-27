a3b451709b1be3986ea3d665533935f6185e8688
sebastian-vivas <seb.vivas.dev@gmail.com>
2020-03-27 10:41:26 -0400

    Roulette Sim

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://sebVivas:sleepTracker@cluster0-isfv7.mongodb.net/roulette?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'roulette'
+};
